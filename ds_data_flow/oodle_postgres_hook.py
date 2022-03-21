f5f4761c67e3571aebf57a6e3a0211ee7008f473
Ezra Citron <ezracitron@gmail.com>
2022-03-21 13:50:17 +0000

    remove license and change paths

--- ds_data_flow/oodle_postgres_hook.py
+++ ds_data_flow/oodle_postgres_hook.py
@@ -1,21 +1,4 @@
-# -*- coding: utf-8 -*-
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+
 
 import os
 import boto3
@@ -28,11 +11,11 @@ import psycopg2
 import logging
 import psycopg2.extensions
 from contextlib import closing
-from oodle.utils.secrets_manager import SecretsManager
+from .secrets_manager import SecretsManager
 from datetime import datetime
 from urllib.parse import quote_plus, urlunsplit
 
-from oodle.utils.xray import XRay
+# from oodle.utils.xray import XRay
 from aws_xray_sdk.core import xray_recorder
 xray_recorder.configure(context_missing='LOG_ERROR')
 logging.getLogger('aws_xray_sdk').setLevel(logging.CRITICAL)
